07bd82ee1d83a117cfc9f3794938c7c129bad5e7
trajashekar423 <trajashekar423@gmail.com>
2024-04-21 16:37:54 +0530

    added new nav itemms

--- myangularresume/src/app/app.component.ts
+++ myangularresume/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent,CommonModule]
 })
 export class AppComponent {
   title = 'myangularresume';
