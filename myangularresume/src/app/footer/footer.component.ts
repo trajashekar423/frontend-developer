44bdb8e7573aa9c4a19ad717228ae932689d5816
trajashekar423 <trajashekar423@gmail.com>
2024-04-21 19:04:57 +0530

    footer

--- myangularresume/src/app/footer/footer.component.ts
+++ myangularresume/src/app/footer/footer.component.ts
@@ -10,5 +10,5 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   footer =['Aboutme','MyResume','MyPortfolio','Contact'];
-  copyright=[© 2024 Company, Inc]
+  copyright=['© 2024 Company, Inc']
 }
