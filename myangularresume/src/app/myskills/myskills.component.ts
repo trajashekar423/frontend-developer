5c452ca2a5a68212dfe9fd18f009a79aa62f0268
trajashekar423 <trajashekar423@gmail.com>
2024-04-23 23:48:51 +0530

    jq

--- myangularresume/src/app/myskills/myskills.component.ts
+++ myangularresume/src/app/myskills/myskills.component.ts
@@ -45,9 +45,15 @@ export class MyskillsComponent implements OnInit {
     },
 
     { 
-   
-      imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/ngrx.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
+         imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/reactjs.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
     },
+
+    { 
+      imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/redux.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
+ },
+ { 
+  imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/nextjslogo.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
+},
     // Add more items as needed
   ];
 item: any;
