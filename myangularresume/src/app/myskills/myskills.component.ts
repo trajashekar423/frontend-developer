7022e1178acb8ba8a1916656111fc8a62a295af6
trajashekar423 <trajashekar423@gmail.com>
2024-04-23 23:51:22 +0530

    jq

--- myangularresume/src/app/myskills/myskills.component.ts
+++ myangularresume/src/app/myskills/myskills.component.ts
@@ -36,6 +36,10 @@ export class MyskillsComponent implements OnInit {
 
     { 
    
+      imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/jq.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
+    },
+    { 
+   
       imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/angularlogo.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0' 
     },
 
