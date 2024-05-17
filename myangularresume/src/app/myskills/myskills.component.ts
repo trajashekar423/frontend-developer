092791f2c3d72fdfac8a1297bf274a2a10ab0908
trajashekar423 <65588319+trajashekar423@users.noreply.github.com>
2024-05-17 10:22:45 +0530

    Update myskills.component.ts
    html5 logo

--- myangularresume/src/app/myskills/myskills.component.ts
+++ myangularresume/src/app/myskills/myskills.component.ts
@@ -25,7 +25,7 @@ export class MyskillsComponent implements OnInit {
   items: CardItem[] = [
     {      
       codetitle:"Html",
-      imageUrl: 'https://dev.azure.com/trajashekar4230786/18f8717f-0bc0-4857-be3d-1485248f36eb/_apis/git/repositories/327409f9-539a-4929-8784-0f3fb5eb454c/items?path=/myangularresume/src/assets/html-5.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0',
+      imageUrl: 'https://www.w3.org/html/logo/img/html5-topper.png',
       url:"#"
     },
 
